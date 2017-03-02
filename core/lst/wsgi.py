a401317caaa52592bbf744a0483b892c3b1263c2
Bertrand PISELLI <bertrand.piselli@smartplace.pro>
2017-03-02 14:44:39 +0100

    WEB init
    solve core docker
    
    IDEs
    
    remove depends
    
    Update README.md
    
    [UPD] Modified to handle ngrx and removed angular from docker
    
    fixtures added
    
    refresh, part 1
    
    removed useless web folder
    
    updated requirements
    
    added 'core' app in INSTALLED_APPS
    
    Added CMD instruction to run the server
    
    Added localhost to ALLOWED_HOSTS
    
    Moved data sample to a higher folder
    
    Rework on README
    
    added pytest
    
    added ENTRYPOINT
    
    Simplification du docker compose
    
    Ajout des instructions de build
    
    updated gif URL

--- core/lst/wsgi.py
+++ core/lst/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gwapit project.
+WSGI config for lst project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gwapit.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lst.settings")
 
 application = get_wsgi_application()
